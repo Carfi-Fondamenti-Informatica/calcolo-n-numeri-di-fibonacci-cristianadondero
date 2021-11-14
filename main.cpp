33728258ba6e8e91db06db07cdc18d404fe66c65
cristianadondero <91587462+cristianadondero@users.noreply.github.com>
2021-11-14 15:16:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, x=1, y=1, z=0;
+    cin>>n;
+    if(n>=2){
+        cout<<x<<endl;
+        for (int i=0;i<n-1;i++){
+            x=y+z;
+            z=y;
+            y=x;
+            cout<<x<<endl;
+        }
+    }else{
+        cout<<"errore";
+    }
+    return 0;
 }
